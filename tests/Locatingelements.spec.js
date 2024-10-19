8fab4465c05453d66e092bfb08224944078adf71
goutham.k@skyblue.aero <goutham.k@skyblue.aero>
2024-10-19 17:12:26 +0530

    Git 1st commit

---
+++ tests/Locatingelements.spec.js
@@ -0,0 +1,12 @@
+const{test, expect} = require('@playwright/test');
+test('Locatingelements',async({page})=>{
+ await page.goto('https://www.demoblaze.com/index.html')
+ const links=await page.$$('a');
+for (const link of links)
+{
+  const linktext= await link.textContent();
+  console.log (linktext);
+}
+
+  })
+
---
+++ tests/locators.spec.js
@@ -0,0 +1,12 @@
+const {test,expect} = require('@playwright/test')
+test('locators',async({page})=>{
+await page.goto("https://www.demoblaze.com/index.html")
+await page.locator('id=login2').click()
+await page.locator('#loginusername').fill("pavanol")
+await page.locator('id=loginpassword').fill("test@123")
+await page.locator("//button[normalize-space()='Log in']").click()
+//Verify logout link presence
+const logoutlink= await page.locator("//a[normalize-space()='Log out']")
+await expect(logoutlink).toBeVisible();
+await page.close()
+})
